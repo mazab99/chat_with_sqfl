f30f49abb53e1d307db87e3fe283f7f2f2fa5aa9
mazab99 <mazab15499@gmail.com>
2023-03-25 20:54:46 +0200

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Chat Room DB',
-        theme: ThemeData(
-            brightness: Brightness.dark,
-            canvasColor: const Color(0xFF222222),
-            scaffoldBackgroundColor: const Color(0xFF090e11),
-            primarySwatch: Colors.blue,
-            dividerColor: const Color(0xFF30383d),
-            inputDecorationTheme: const InputDecorationTheme(
-                filled: true,
-                hintStyle: TextStyle(fontWeight: FontWeight.w100),
-                contentPadding: EdgeInsets.only(top: 5, left: 20),
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                fillColor: Color(0xFF33383B))),
-        home: Scaffold(
-            backgroundColor: const Color(0xFF090e11),
-            body: Stack(
-              children: [
-                LayoutBuilder(builder: (context, constraints) {
-                  return Image.asset("assets/images/background.jpg",
-                      fit: BoxFit.cover,
-                      height: constraints.maxHeight,
-                      width: constraints.maxWidth);
-                }),
-                const ChatContainer(),
-              ],
-            )));
+      title: 'Chat Room DB',
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          canvasColor: const Color(0xFF222222),
+          scaffoldBackgroundColor: const Color(0xFF090e11),
+          primarySwatch: Colors.blue,
+          dividerColor: const Color(0xFF30383d),
+          inputDecorationTheme: const InputDecorationTheme(
+              filled: true,
+              hintStyle: TextStyle(fontWeight: FontWeight.w100),
+              contentPadding: EdgeInsets.only(top: 5, left: 20),
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              fillColor: Color(0xFF33383B))),
+      home: Scaffold(
+        backgroundColor: const Color(0xFF090e11),
+        body: Stack(
+          children: [
+            LayoutBuilder(builder: (context, constraints) {
+              return Image.asset("assets/images/background.jpg",
+                  fit: BoxFit.cover,
+                  height: constraints.maxHeight,
+                  width: constraints.maxWidth);
+            }),
+            const ChatContainer(),
+          ],
+        ),
+      ),
+    );
   }
 }
